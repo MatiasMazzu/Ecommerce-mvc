c38c9c927ebce791c5af19be286b0dfda22a73e7
FacuuLopez <faculopez93@hotmail.com.ar>
2022-08-31 14:36:31 -0300

    Creacion de las clases Categoria y Sucursal con sus propiedades basicas (falta agregar Ids)

---
+++ Carrito-C/Carrito-C/Models/Categoriacs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace Carrito_C.Models
+{
+    public class Categoriacs
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        List<Producto> Productos;
+    }
+
+}
