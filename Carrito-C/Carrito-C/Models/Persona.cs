5817c44f1f141befd6dd51fb61463aa058ea64bd
FacuuLopez <faculopez93@hotmail.com.ar>
2022-08-31 21:37:47 -0300

    Agregue atributos basicos a Persona

--- Carrito-C/Carrito-C/Models/Persona.cs
+++ Carrito-C/Carrito-C/Models/Persona.cs
@@ -2,5 +2,11 @@
 {
     public class Persona
     {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Telefono { get; set; }
+        public string Direccion { get; set; }
+        public string Email { get; set; }
+        public DateOnly FechaAlta { get; set; }
     }
 }
