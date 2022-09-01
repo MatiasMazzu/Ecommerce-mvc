7cc169df1a94a7dab3f180b0736bec691f786064
YamiMartin <yamimartin@gmail.com>
2022-09-01 20:10:38 -0300

    agregue funciones a Stock y StockItem

--- Carrito-C/Carrito-C/Models/Stock.cs
+++ Carrito-C/Carrito-C/Models/Stock.cs
@@ -1,6 +1,16 @@
-﻿namespace Carrito_C.Models
+﻿using Carrito_C.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace Carrito_C.Models
 {
     public class Stock
     {
+
+        public int Id { get; set; }
+        [StringLength(50, MinimumLength = 2, ErrorMessage = MsgError.ErrorNombres)]
+        public List <Producto> Productos { get; set; }
+        public int Cantidad { get; set; }
+
+
     }
 }
--- Carrito-C/Carrito-C/Models/StockItem.cs
+++ Carrito-C/Carrito-C/Models/StockItem.cs
@@ -1,6 +1,14 @@
-﻿namespace Carrito_C.Models
+﻿using Carrito_C.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace Carrito_C.Models
 {
     public class StockItem
     {
+        public int Id { get; set; }
+        [StringLength(50, MinimumLength = 2, ErrorMessage = MsgError.ErrorNombres)]
+        public List<Producto> Productos { get; set; }
+        public List<Sucursal> Sucursales { get; set; }
+        public int Cantidad { get; set; }
     }
 }
