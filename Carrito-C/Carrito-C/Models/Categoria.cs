b58370893cf5609e40475c4cf06de996cffe8e3f
Matias Mazzucchelli <matias.mazzucchelli2@globallogic.com>
2022-09-02 16:04:44 -0300

    Se creo la clase Carrito y Compras, se agregaron validaciones y propiedades a las clases Cliente, Producto, Usuario, Empleado. Se fixeo un ErrorMessage comun a varias clases y se agregaron nuevos mensajes de error.

--- Carrito-C/Carrito-C/Models/Categoria.cs
+++ Carrito-C/Carrito-C/Models/Categoria.cs
@@ -6,7 +6,9 @@ namespace Carrito_C.Models
     public class Categoria
     {
         public int Id { get; set; }
-        [StringLength(50,MinimumLength =2, ErrorMessage=MsgError.ErrorNombres)]
+        [Required(ErrorMessage = MsgError.Requerido)]
+        [MaxLength(50, ErrorMessage = MsgError.MsgMaxStr)]
+        [MinLength(2, ErrorMessage = MsgError.MsgMinStr)]
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
         List<Producto> Productos;
