b5ccde3391c87d302ef1e969e0125147fbfdce8b
andresdtcity <andresdtcity@hotmail.com>
2022-09-02 15:23:48 -0300

    test commit

--- Carrito-C/Carrito-C/Models/Carrito.cs
+++ Carrito-C/Carrito-C/Models/Carrito.cs
@@ -2,5 +2,15 @@
 {
     public class Carrito
     {
+        public int Id { get; set; }
+        public String activo { get; set; }
+        public String cliente { get; set; }
+        public String carritoItem { get; set; }
+        public int subtotal { get; set; }
+
+        
+         
+         
+
     }
 }
