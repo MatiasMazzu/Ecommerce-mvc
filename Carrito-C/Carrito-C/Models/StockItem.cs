c6f4e0665fa7d8d0b220b4f84d3d7b09819d540b
YamiMartin <yamimartin@gmail.com>
2022-09-09 20:44:59 -0300

    agregue los required de StockItem, hice modificaciones apegandome a la consigna, relacione clase con Sucursal y Producto,

--- Carrito-C/Carrito-C/Models/StockItem.cs
+++ Carrito-C/Carrito-C/Models/StockItem.cs
@@ -6,9 +6,20 @@ namespace Carrito_C.Models
     public class StockItem
     {
         public int Id { get; set; }
-        [StringLength(50, MinimumLength = 2, ErrorMessage = MsgError.CommonError)]
-        public List<Producto> Productos { get; set; }
-        public List<Sucursal> Sucursales { get; set; }
+
+        [Required(ErrorMessage = MsgError.Requerido)]
+        public int ProductoId { get; set; } 
+        public Producto Producto { get; set; }
+
+
+        [Required(ErrorMessage = MsgError.Requerido)]
+        public int SucursalId { get; set; } 
+        public Sucursal Sucursal { get; set; }
+
+        [Required(ErrorMessage = MsgError.Requerido)]
+        [Range(1, 199999999, ErrorMessage = MsgError.CommonError2)]
         public int Cantidad { get; set; }
+
+
     }
 }
