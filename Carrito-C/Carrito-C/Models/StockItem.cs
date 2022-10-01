7e8a407d82900a744456588f7ee20c743983fc6a
YamiMartin <yamimartin@gmail.com>
2022-10-01 19:56:52 -0300

    Segun Indicaciones, elimite Id de StockItem para evitar complicaciones futuras

--- Carrito-C/Carrito-C/Models/StockItem.cs
+++ Carrito-C/Carrito-C/Models/StockItem.cs
@@ -5,8 +5,7 @@ namespace Carrito_C.Models
 {
     public class StockItem
     {
-        public int Id { get; set; }
-        [StringLength(50, MinimumLength = 2, ErrorMessage = MsgError.CommonError)]
+
         public List<Producto> Productos { get; set; }
         public List<Sucursal> Sucursales { get; set; }
         public int Cantidad { get; set; }
