610228494b9bc29897a892173caeadc8ac0d3861
andresdtcity <andresdtcity@hotmail.com>
2022-10-19 20:19:09 -0300

    cambio24

---
+++ Carrito-C/Carrito-C/Controllers/PersonasController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Carrito_C.Data;
+using Carrito_C.Models;
+
+namespace Carrito_C.Controllers
+{
+    public class PersonasController : Controller
+    {
+        private readonly CarritoCContext _context;
+
+        public PersonasController(CarritoCContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Personas
+        public  IActionResult Index()
+        {
+              return View(_context.Personas.ToList());
+        }
+
+        // GET: Personas/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null || _context.Personas == null)
+            {
+                return NotFound();
+            }
+
+            var persona = _context.Personas
+                .FirstOrDefault(p => p.Id == id);
+            if (persona == null)
+            {
+                return NotFound();
+            }
+
+            return View(persona);
+        }
+
+        // GET: Personas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Personas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,Apellido,Dni,Telefono,Direccion,Email,FechaAlta,UserName,Password")] Persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(persona);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(persona);
+        }
+
+        // GET: Personas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Personas == null)
+            {
+                return NotFound();
+            }
+
+            var persona = await _context.Personas.FindAsync(id);
+            if (persona == null)
+            {
+                return NotFound();
+            }
+            return View(persona);
+        }
+
+        // POST: Personas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Apellido,Dni,Telefono,Direccion,Email,FechaAlta,UserName,Password")] Persona persona)
+        {
+            if (id != persona.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(persona);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PersonaExists(persona.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(persona);
+        }
+
+        // GET: Personas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Personas == null)
+            {
+                return NotFound();
+            }
+
+            var persona = await _context.Personas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (persona == null)
+            {
+                return NotFound();
+            }
+
+            return View(persona);
+        }
+
+        // POST: Personas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Personas == null)
+            {
+                return Problem("Entity set 'CarritoCContext.Personas'  is null.");
+            }
+            var persona = await _context.Personas.FindAsync(id);
+            if (persona != null)
+            {
+                _context.Personas.Remove(persona);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PersonaExists(int id)
+        {
+          return _context.Personas.Any(e => e.Id == id);
+        }
+    }
+}
