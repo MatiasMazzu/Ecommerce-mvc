86c461bde5ad4184beaa75309eb760e886bec1a2
FacuuLopez <faculopez93@hotmail.com.ar>
2022-10-29 22:39:22 -0300

    Agregado PreCarga(roles)

---
+++ Carrito-C/Carrito-C/Controllers/PreCarga.cs
@@ -0,0 +1,60 @@
+﻿using Carrito_C.Data;
+using Carrito_C.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Carrito_C.Controllers
+{
+    public class PreCarga : Controller
+    {
+        private readonly UserManager<Persona> _userManager;
+        private readonly RoleManager<Rol> _roleManager;
+        private readonly CarritoCContext _context;
+
+        private List<String> roles = new List<String>() { "Admin", "Cliente", "Empleado", "usuario" };
+        public PreCarga(UserManager<Persona> userManager ,RoleManager<Rol> roleManager, CarritoCContext context)
+        {
+            this._userManager = userManager;
+            this._roleManager = roleManager;
+            this._context = context;
+
+        }
+        public IActionResult Seed()
+        {
+            CrearRoles().Wait();
+            CrearAdmin().Wait();
+            CrearClientes().Wait();
+            CrearEmpleados().Wait();
+
+
+            return RedirectToAction("Index", "Home", new { mensaje = "Proceso Seed finalizado" });
+        }
+
+        private async Task CrearEmpleados()
+        {
+            
+        }
+
+        private async Task CrearClientes()
+        {
+          
+        }
+
+        private async Task CrearAdmin()
+        {
+            
+        }
+
+        private async Task CrearRoles()
+        {
+            foreach(var rolName in roles)
+            {
+                if (!await _roleManager.RoleExistsAsync(rolName))
+                {
+                    await _roleManager.CreateAsync(new Rol(rolName));
+                }
+            }
+        }
+    }
+}
